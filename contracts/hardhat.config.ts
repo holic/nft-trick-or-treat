cff4f171f8931ae1be80896e8bc71b5d22aedf5a
Kevin Ingersoll <kingersoll@gmail.com>
2021-10-19 23:55:52 -0700

    randomize which wallet the tx is sent to

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,8 +2,21 @@ import "@typechain/hardhat";
 import "@nomiclabs/hardhat-ethers";
 import "@nomiclabs/hardhat-waffle";
 import "@openzeppelin/hardhat-upgrades";
+import { task } from "hardhat/config";
+import { ethers } from "ethers";
 
 require("dotenv").config();
+
+task("createWallet", "Creates a new wallet").setAction(async () => {
+  console.log("");
+  new Array(5).fill(0).map(() => {
+    const wallet = ethers.Wallet.createRandom();
+    console.log("Wallet:\t", wallet.address);
+    console.log("Private key:\t", wallet.privateKey.replace(/^0x/, ""));
+    console.log("");
+  });
+  console.log("");
+});
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
